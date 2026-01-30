bc6b217bbe4b4227d0be9811dd3e5af52add4e08
pzmique <152024423+pzmique@users.noreply.github.com>
2026-01-30 18:53:29 -0300

    Add files via upload

---
+++ assets/js/IntersectionObserverpage.js
@@ -0,0 +1,14 @@
+const elements = document.querySelectorAll('.fade-in')
+
+const observer = new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+                observer.unobserve(entry.target);
+            }
+        })
+    }, { threshold: 0.1 }
+);
+
+elements.forEach(el => observer.observe(el));
