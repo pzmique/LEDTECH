885cf3581cb93b950f565992677bac6dd25c5251
pzmique <152024423+pzmique@users.noreply.github.com>
2026-02-02 23:25:21 -0300

    Update drop-down-menu.js

--- assets/js/drop-down-menu.js
+++ assets/js/drop-down-menu.js
@@ -6,6 +6,18 @@ btnMenu.addEventListener("click", (e) => {
     e.stopPropagation();
 
     menu.classList.toggle("open")
+    menu.animate (
+        [
+            { transform: 'translateY(50px)' },
+            { transform: 'translateY(0)' },
+            { transform: 'translateY(10px)'},
+            { transform: 'translateY(0)' }
+        ],
+        {
+            duration: 500,
+            easing: 'ease'
+        }
+    )
 });
 
 document.addEventListener("click", () => {
